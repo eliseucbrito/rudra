903edb77772f95beeb98165537718d126d8b6563
Eliseu C. Brito <ecb2@cin.ufpe.br>
2025-06-17 22:36:45 -0300

    feat: update home.nix for gnome

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -13,34 +13,21 @@ in {
     stateVersion = stateVersion;
 
     file = {
-      # Hyprland Config
-      ".config/hypr".source = ../../dotfiles/.config/hypr;
-
-      # wlogout icons
-      ".config/wlogout/icons".source = ../../config/wlogout;
-
       # Top Level Files symlinks
       ".zshrc".source = ../../dotfiles/.zshrc;
       ".zshenv".source = ../../dotfiles/.zshenv;
-      ".xinitrc".source = ../../dotfiles/.xinitrc;
       ".gitconfig".source = ../../dotfiles/.gitconfig;
       ".ideavimrc".source = ../../dotfiles/.ideavimrc;
-      ".nirc".source = ../../dotfiles/.nirc;
       ".local/bin/wallpaper".source = ../../dotfiles/.local/bin/wallpaper;
 
       # Config directories
-      ".config/alacritty".source = ../../dotfiles/.config/alacritty;
-      ".config/dunst".source = ../../dotfiles/.config/dunst;
       ".config/fastfetch".source = ../../dotfiles/.config/fastfetch;
       ".config/kitty".source = ../../dotfiles/.config/kitty;
       ".config/mpv".source = ../../dotfiles/.config/mpv;
       ".config/tmux/tmux.conf".source = ../../dotfiles/.config/tmux/tmux.conf;
-      ".config/waybar".source = ../../dotfiles/.config/waybar;
       ".config/yazi".source = ../../dotfiles/.config/yazi;
-      ".config/wezterm".source = ../../dotfiles/.config/wezterm;
 
       # Individual config files
-      ".config/kwalletrc".source = ../../dotfiles/.config/kwalletrc;
       ".config/starship.toml".source = ../../dotfiles/.config/starship.toml;
       ".config/mimeapps.list".source = ../../dotfiles/.config/mimeapps.list;
     };
@@ -50,8 +37,6 @@ in {
       pkgs.appimage-run
       (import ../../scripts/rofi-launcher.nix {inherit pkgs;})
     ];
-
-    # lib.attrValues myPackages
 
     sessionPath = [
       "$HOME/.local/bin"
@@ -64,8 +49,6 @@ in {
     ../../config/wlogout.nix
   ];
 
-  # Styling
-  stylix.targets.waybar.enable = false;
   gtk = {
     iconTheme = {
       name = "Papirus-Dark";
@@ -82,27 +65,6 @@ in {
     enable = true;
     style.name = "adwaita-dark";
     platformTheme.name = "gtk3";
-  };
-
-  services.hypridle = {
-    settings = {
-      general = {
-        after_sleep_cmd = "hyprctl dispatch dpms on";
-        ignore_dbus_inhibit = false;
-        lock_cmd = "hyprlock";
-      };
-      listener = [
-        {
-          timeout = 900;
-          on-timeout = "hyprlock";
-        }
-        {
-          timeout = 1200;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
-        }
-      ];
-    };
   };
 
   programs.home-manager.enable = true;
